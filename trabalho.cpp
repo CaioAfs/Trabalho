8989187fb05db30a9a596ae176d951d2c59dc508
caio araujo <Caioafsouza1@gmail.com>
2022-06-04 13:38:34 -0300

    Primeira versão do trabalho

---
+++ trabalho.cpp
@@ -0,0 +1,80 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+typedef struct date{
+    int day;
+    int month;
+    int year;
+}Date;
+
+typedef struct client {
+    int code;
+    string name;
+    bool active;
+
+    void print(){
+     cout<<"Codigo: "<< code << ";"<< " Nome: " << name << ";" << " Ativo: "
+    << (active  ? "Sim" : "Nao" ) << ";"<<endl;
+    }
+}Client;
+
+typedef struct fuel{
+    int code;
+    string name;
+    float price;
+    Date changeDatePrice;
+
+    void print(){
+        cout<<"Codigo: " << code << ";" << " Nome: " << name << ";" << " Preco: " << price << ";"<<
+        " Data de alteracao: "<< changeDatePrice.day <<"/"<< changeDatePrice.month <<"/" <<
+         changeDatePrice.year << ";"<<endl;
+    }
+}Fuel;
+
+
+
+void init(map<int,Client>& c, map<int,Fuel>& f);
+void printClient(map<int,Client>&c);
+
+int main() {
+    map<int,Client>client;
+    map<int,Fuel>fuel;
+
+    init(client,fuel);
+
+    cout << "\n  CLIENTES \n\n";
+    for(auto i: client){
+        if(i.second.active){
+        i.second.print();
+        }
+    }
+
+}
+
+
+
+
+
+void init(map<int,Client>& c,map<int,Fuel>& f){
+    Client client;
+
+    client.code = 2235;
+    c[client.code].code = 45265;
+    c[client.code].name = "Hugo Ferreira";
+    c[client.code].active = true;
+
+    client.code = 4444;
+    c[client.code].code = 48964;
+    c[client.code].name = "Vitor alexandre";
+    c[client.code].active = false;
+
+    client.code = 2877;
+    c[client.code].code = 66958;
+    c[client.code].name = "Fulano Ciclano";
+    c[client.code].active = true;
+
+}
