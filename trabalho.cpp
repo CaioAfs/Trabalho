60551679575484e580be4b4075c39aa90f00171c
Hugo Ferreira Da Silva <81571386+hugoleao02@users.noreply.github.com>
2022-06-05 11:12:18 -0300

    Update trabalho.cpp
    adicionei struct sales criei função lista combustivel e clientes

--- trabalho.cpp
+++ trabalho.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 typedef struct date{
     int day;
-    int month;
+    double month;
     int year;
 }Date;
 
@@ -17,7 +17,7 @@ typedef struct client {
     bool active;
 
     void print(){
-     cout<<"Codigo: "<< code << ";"<< " Nome: " << name << ";" << " Ativo: "
+     cout<<"Codigo: "<< code << ","<< " Nome: " << name << "," << " Ativo: "
     << (active  ? "Sim" : "Nao" ) << ";"<<endl;
     }
 }Client;
@@ -29,15 +29,37 @@ typedef struct fuel{
     Date changeDatePrice;
 
     void print(){
-        cout<<"Codigo: " << code << ";" << " Nome: " << name << ";" << " Preco: " << price << ";"<<
+        cout<<"Codigo: " << code << "," << " Nome: " << name << "," << " Preco: " << price << ","<<
         " Data de alteracao: "<< changeDatePrice.day <<"/"<< changeDatePrice.month <<"/" <<
          changeDatePrice.year << ";"<<endl;
     }
 }Fuel;
 
+typedef struct sales{
+    Client codeClient;
+    Fuel codeFuel;
+    bool paidOut;
+    double theAmount;
+    double literValue;
+    double total;
+    string responsible;
+    Date date;
+
+
+
+    void print(){
+        cout << "Data: " << date.day <<"/"<< date.month <<"/" << date.year<< ", Codigo do cliente: "
+        << codeClient.code << ", Codigo do combustivel: "<< codeFuel.code<< ", Quantidade: " << theAmount
+        << ", Valor do litro: " << literValue << ", Total: "<< total << ", Reponsavel: "<< responsible
+        << ", Pago: " << paidOut << ";"<< endl;
+        }
+}Sales;
+
 
 void init(map<int,Client>& c, map<int,Fuel>& f);
 void printClient(map<int,Client>&c);
+void listClient(map<int,Client>& c);
+void listFuel(map<int,Fuel>& f);
 
 int main() {
 
@@ -53,23 +75,17 @@ int main() {
     map<int,Client>client;
     map<int,Fuel>fuel;
 
-    init(client,fuel);
 
-    cout << "\n  CLIENTES \n\n";
-    for(auto i: client){
-        if(i.second.active){
-        i.second.print();
-        }
-    }
+    init(client,fuel);
+    listClient(client);
+    listFuel(fuel);
 
 }
 
 
-
-
-
 void init(map<int,Client>& c,map<int,Fuel>& f){
     Client client;
+    Fuel fuel;
 
     client.code = 2235;
     c[client.code].code = 45265;
@@ -86,4 +102,35 @@ void init(map<int,Client>& c,map<int,Fuel>& f){
     c[client.code].name = "Fulano Ciclano";
     c[client.code].active = true;
 
+    fuel.code = 4558;
+    f[fuel.code ].code = 68745;
+    f[fuel.code ].name = "Diesel s10";
+    f[fuel.code ].price = 6.25;
+    f[fuel.code ].changeDatePrice.day = 26;
+    f[fuel.code ].changeDatePrice.month = 06;
+    f[fuel.code ].changeDatePrice.year = 2021;
+
+    fuel.code = 6489;
+    f[fuel.code ].code = 45689;
+    f[fuel.code ].name = "Gasolina";
+    f[fuel.code ].price = 7.49;
+    f[fuel.code ].changeDatePrice.day = 31;
+    f[fuel.code ].changeDatePrice.month = 11;
+    f[fuel.code ].changeDatePrice.year = 2021;
+}
+
+void listFuel(map<int,Fuel>& f){
+    cout << "\n  Combustivel \n\n";
+    for(auto i: f){
+        i.second.print();
+    }
+}
+
+void listClient(map<int,Client>& c){
+    cout << "\n  CLIENTES \n\n";
+    for(auto i: c){
+        if(i.second.active){
+        i.second.print();
+        }
+    }
 }
